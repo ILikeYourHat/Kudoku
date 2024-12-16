ed147ea06b2232c96f1229d8fa51c46c0359652e
Marcin <marcin.laskowski.07@gmail.com>
2024-12-16 22:32:29 +0100

    Add stub for publishing artifacts

--- kudoku/build.gradle.kts
+++ kudoku/build.gradle.kts
@@ -1,8 +1,10 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     alias(libs.plugins.kotlin)
     alias(libs.plugins.detekt)
     alias(libs.plugins.kover)
-    alias(libs.plugins.kudoku.publishing)
+    id("com.vanniktech.maven.publish") version "0.30.0"
 }
 
 kotlin {
@@ -42,4 +44,32 @@ dependencies {
     testImplementation(libs.junit5.params)
     testImplementation(libs.kotest)
     testImplementation(libs.mockk)
+}
+
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+    signAllPublications()
+    coordinates("io.github.ilikeyourhat.kudoku", "kudoku", "0.0.1")
+    pom {
+        name = "Kudoku"
+        description.set("A sudoku engine written in Kotlin")
+        url.set("https://github.com/ILikeYourHat/Kudoku")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        developers {
+            developer {
+                id.set("ILikeYourHat")
+                name.set("Marcin Laskowski")
+            }
+        }
+        scm {
+            connection.set("git@github.com:ILikeYourHat/Kudoku.git")
+            developerConnection.set("git@github.com:ILikeYourHat/Kudoku.git")
+            url.set("https://github.com/ILikeYourHat/Kudoku")
+        }
+    }
 }
