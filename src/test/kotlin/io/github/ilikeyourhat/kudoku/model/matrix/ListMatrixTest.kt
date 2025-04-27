facd8beb046933e96afea93c33c2a11e8506c157
Marcin Laskowski <marcin.laskowski.07@gmail.com>
2025-04-27 11:29:47 +0200

    Added better test coverage for ListMatrix (#100)
    * Added better test coverage for ListMatrix
    
    * Code style fixes
    
    * Grammar fix

--- src/test/kotlin/io/github/ilikeyourhat/kudoku/model/matrix/ListMatrixTest.kt
+++ src/test/kotlin/io/github/ilikeyourhat/kudoku/model/matrix/ListMatrixTest.kt
@@ -1,85 +1,119 @@
 package io.github.ilikeyourhat.kudoku.model.matrix
 
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertFalse
-import org.junit.jupiter.api.Assertions.assertNotEquals
-import org.junit.jupiter.api.Assertions.assertNull
-import org.junit.jupiter.api.Assertions.assertSame
-import org.junit.jupiter.api.Assertions.assertTrue
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.matchers.booleans.shouldBeFalse
+import io.kotest.matchers.booleans.shouldBeTrue
+import io.kotest.matchers.collections.shouldBeEmpty
+import io.kotest.matchers.collections.shouldContainOnly
+import io.kotest.matchers.collections.shouldContainOnlyNulls
+import io.kotest.matchers.collections.shouldNotBeEmpty
+import io.kotest.matchers.equals.shouldBeEqual
+import io.kotest.matchers.equals.shouldNotBeEqual
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.throwable.shouldHaveMessage
+import io.kotest.matchers.types.shouldHaveSameHashCodeAs
+import io.kotest.matchers.types.shouldNotHaveSameHashCodeAs
 import org.junit.jupiter.api.Test
 
 class ListMatrixTest {
 
     @Test
-    fun createEmptyMatrixAndCheckItsSize() {
+    fun `create empty matrix and check its size`() {
         val matrix = ListMatrix(0, 0, null)
-        assertEquals(0, matrix.sizeX)
-        assertEquals(0, matrix.sizeY)
-        assertTrue(matrix.isEmpty())
+
+        matrix.sizeX.shouldBeEqual(0)
+        matrix.sizeY.shouldBeEqual(0)
+        matrix.shouldBeEmpty()
     }
 
     @Test
-    fun createNonEmptyMatrixAndCheckItsSize() {
+    fun `create non empty matrix and check its size`() {
         val matrix = ListMatrix(4, 7, null)
-        assertEquals(4, matrix.sizeX)
-        assertEquals(7, matrix.sizeY)
-        assertFalse(matrix.isEmpty())
+
+        matrix.sizeX.shouldBeEqual(4)
+        matrix.sizeY.shouldBeEqual(7)
+        matrix.shouldNotBeEmpty()
     }
 
     @Test
-    fun checkIfMatrixIsInitializedWithNulls() {
-        val matrix = ListMatrix(2, 2, null)
-        assertNull(matrix[0, 0])
-        assertNull(matrix[1, 0])
-        assertNull(matrix[0, 1])
-        assertNull(matrix[1, 1])
+    fun `check if matrix is initialized with nulls`() {
+        val matrix: ListMatrix<Any?> = ListMatrix(2, 2, null)
+
+        matrix.shouldContainOnlyNulls()
     }
 
     @Test
-    fun checkIfMatrixIsInitializedWithDefaultValue() {
+    fun `check if matrix is initialized with default value`() {
         val matrix = ListMatrix(2, 2, "test")
-        assertEquals("test", matrix[0, 0])
-        assertEquals("test", matrix[1, 0])
-        assertEquals("test", matrix[0, 1])
-        assertEquals("test", matrix[1, 1])
+
+        matrix.shouldContainOnly("test")
     }
 
     @Test
-    fun insertObjectIntoMatrixAndCheckItsThere() {
+    fun `insert object into matrix and check it's there`() {
         val matrix = ListMatrix<Any?>(3, 3, null)
         val any = Any()
 
         matrix[1, 2] = any
-        assertSame(any, matrix[1, 2])
+
+        matrix[1, 2].shouldBe(any)
     }
 
     @Test
-    fun checkIfMatrixContainsGivenElement() {
+    fun `check if matrix contains given element`() {
         val matrix = ListMatrix(2, 2, 0)
-        val testValue = 7
 
-        assertFalse(matrix.contains(testValue))
+        matrix.contains(7)
+            .shouldBeFalse()
 
-        matrix[1, 1] = testValue
+        matrix[1, 1] = 7
 
-        assertTrue(matrix.contains(testValue))
+        matrix.contains(7)
+            .shouldBeTrue()
     }
 
     @Test
-    fun checkIfMatrixIsEqualsWhenSizeIsEquals() {
-        val smallMatrix = ListMatrix(2, 2, null)
-        val otherSmallMatrix = ListMatrix(2, 2, null)
-        val veryBigMatrix = ListMatrix(1000, 1000, null)
+    fun `check if matrix contains all of the given elements`() {
+        val matrix = ListMatrix(2, 2, 0)
 
-        assertEquals(smallMatrix, otherSmallMatrix)
-        assertNotEquals(smallMatrix, veryBigMatrix)
+        matrix.containsAll(listOf(7, 8))
+            .shouldBeFalse()
 
-        assertEquals(smallMatrix.hashCode(), otherSmallMatrix.hashCode())
-        assertNotEquals(smallMatrix.hashCode(), veryBigMatrix.hashCode())
+        matrix[1, 1] = 7
+        matrix[0, 1] = 8
+
+        matrix.containsAll(listOf(7, 8))
+            .shouldBeTrue()
     }
 
     @Test
-    fun checkIfMatrixIsEqualsWhenDataIsEquals() {
+    fun `check basic equals contract`() {
+        val matrix = ListMatrix(2, 2, null)
+        val sameMatrix = ListMatrix(2, 2, null)
+
+        matrix.shouldBeEqual(sameMatrix)
+        matrix.shouldHaveSameHashCodeAs(sameMatrix)
+        matrix.shouldNotBeEqual("a string")
+    }
+
+    @Test
+    fun `check if matrix is equal when size is equal`() {
+        val matrix = ListMatrix(2, 2, null)
+        val equalMatrix = ListMatrix(2, 2, null)
+        val differentMatrix = ListMatrix(1000, 1000, null)
+        val matrixWithDifferentSizeY = ListMatrix(2, 3, null)
+
+        matrix.shouldBeEqual(equalMatrix)
+        matrix.shouldNotBeEqual(differentMatrix)
+        matrix.shouldNotBeEqual(matrixWithDifferentSizeY)
+
+        matrix.shouldHaveSameHashCodeAs(equalMatrix)
+        matrix.shouldNotHaveSameHashCodeAs(differentMatrix)
+        matrix.shouldNotHaveSameHashCodeAs(matrixWithDifferentSizeY)
+    }
+
+    @Test
+    fun `check if matrix is equal when data is equal`() {
         val twinMatrix1 = ListMatrix(2, 2, "")
         twinMatrix1[0, 0] = "twin"
 
@@ -92,25 +126,59 @@ class ListMatrixTest {
         val differentMatrix2 = ListMatrix(2, 2, "")
         differentMatrix2[1, 1] = "twin"
 
-        assertEquals(twinMatrix1, twinMatrix2)
-        assertNotEquals(twinMatrix1, differentMatrix1)
-        assertNotEquals(twinMatrix1, differentMatrix2)
+        twinMatrix1.shouldBeEqual(twinMatrix2)
+        twinMatrix1.shouldNotBeEqual(differentMatrix1)
+        twinMatrix1.shouldNotBeEqual(differentMatrix2)
 
-        assertEquals(twinMatrix1.hashCode(), twinMatrix2.hashCode())
-        assertNotEquals(twinMatrix1.hashCode(), differentMatrix1.hashCode())
-        assertNotEquals(twinMatrix1.hashCode(), differentMatrix2.hashCode())
+        twinMatrix1.shouldHaveSameHashCodeAs(twinMatrix2)
+        twinMatrix1.shouldNotHaveSameHashCodeAs(differentMatrix1)
+        twinMatrix1.shouldNotHaveSameHashCodeAs(differentMatrix2)
     }
 
     @Test
-    fun checkIfCoordinatesByIndexWorks() {
+    @Suppress("ArgumentListWrapping") // for better coordinates readability
+    fun `check if coordinates by index works`() {
         val matrix = ListMatrix(3, 2, "")
 
         val coordinates = (0 until matrix.size).toList()
             .map { index -> matrix.coordinatesOf(index) }
 
-        assertEquals(
-            listOf(0 to 0, 1 to 0, 2 to 0, 0 to 1, 1 to 1, 2 to 1),
-            coordinates
+        coordinates.shouldBeEqual(
+            listOf(
+                0 to 0, 1 to 0, 2 to 0,
+                0 to 1, 1 to 1, 2 to 1
+            )
         )
+    }
+
+    @Test
+    fun `should throw exception when creating matrix with negative size`() {
+        shouldThrow<IllegalArgumentException> { ListMatrix(-1, 0, "") }
+            .shouldHaveMessage("sizeX must be non-negative")
+        shouldThrow<IllegalArgumentException> { ListMatrix(0, -1, "") }
+            .shouldHaveMessage("sizeY must be non-negative")
+    }
+
+    @Test
+    fun `should throw exception when creating matrix with incorrect data size`() {
+        shouldThrow<IllegalArgumentException> { ListMatrix(2, 2, listOf(1, 2)) }
+            .shouldHaveMessage("Data size must be equal to 4, but was 2")
+        shouldThrow<IllegalArgumentException> { ListMatrix(2, 2, listOf(1, 2, 3, 4, 5)) }
+            .shouldHaveMessage("Data size must be equal to 4, but was 5")
+    }
+
+    @Test
+    fun `should throw exception when coordinates are out of bounds`() {
+        val matrix = ListMatrix(3, 2, "")
+
+        shouldThrow<IndexOutOfBoundsException> { matrix[3, 0] }
+            .shouldHaveMessage("x must be in range [0, 2], but was 3")
+        shouldThrow<IndexOutOfBoundsException> { matrix[0, 3] }
+            .shouldHaveMessage("y must be in range [0, 1], but was 3")
+
+        shouldThrow<IndexOutOfBoundsException> { matrix[3, 0] = "test" }
+            .shouldHaveMessage("x must be in range [0, 2], but was 3")
+        shouldThrow<IndexOutOfBoundsException> { matrix[0, 3] = "test" }
+            .shouldHaveMessage("y must be in range [0, 1], but was 3")
     }
 }
