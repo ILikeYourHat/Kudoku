9d7d93b987adaf9a9288b64329634959f8fb11ad
Marcin <marcin.laskowski.07@gmail.com>
2025-05-24 15:06:20 +0200

    Clear project from Java standard library references

--- core/src/main/kotlin/io/github/ilikeyourhat/kudoku/parsing/SudokuTextFormatParser.kt
+++ core/src/main/kotlin/io/github/ilikeyourhat/kudoku/parsing/SudokuTextFormatParser.kt
@@ -4,40 +4,22 @@ import io.github.ilikeyourhat.kudoku.model.JigsawSudokuType
 import io.github.ilikeyourhat.kudoku.model.Sudoku
 import io.github.ilikeyourhat.kudoku.model.SudokuType
 import io.github.ilikeyourhat.kudoku.type.TypesRegistry
-import java.io.File
-import java.io.FileNotFoundException
-import java.util.*
-import java.util.regex.Pattern
 
 class SudokuTextFormatParser {
 
     fun parseOne(text: String): Sudoku {
-        return Command(text).parseOne()
+        return Command(text.lineSequence()).parseOne()
     }
 
     fun parseMany(text: String): List<Sudoku> {
-        return Command(text).parseMany()
-    }
-
-    @Throws(FileNotFoundException::class)
-    fun parseOne(file: File): Sudoku {
-        return Command(file).parseOne()
-    }
-
-    @Throws(FileNotFoundException::class)
-    fun parseMany(file: File): List<Sudoku> {
-        return Command(file).parseMany()
+        return Command(text.lineSequence()).parseMany()
     }
 
     private inner class Command {
-        private val scanner: Scanner
+        private val lines: Iterator<String>
 
-        constructor(text: String) {
-            scanner = Scanner(text)
-        }
-
-        constructor(file: File) {
-            scanner = Scanner(file)
+        constructor(text: Sequence<String>) {
+            lines = text.iterator()
         }
 
         fun parseMany(): List<Sudoku> {
@@ -45,15 +27,12 @@ class SudokuTextFormatParser {
             do {
                 val sudoku = parse()
                 sudokuList.add(sudoku)
-            } while (scanner.hasNext())
-            scanner.close()
+            } while (lines.hasNext())
             return sudokuList
         }
 
         fun parseOne(): Sudoku {
-            val sudoku = parse()
-            scanner.close()
-            return sudoku
+            return parse()
         }
 
         private fun parse(): Sudoku {
@@ -68,14 +47,14 @@ class SudokuTextFormatParser {
             val expectedValuesCount = getExpectedValuesCount(type)
             var y = 0
             while (y < height) {
-                var line = scanner.nextLine()
+                var line = lines.next()
                 while (line.isEmpty()) {
-                    line = scanner.nextLine()
+                    line = lines.next()
                 }
-                val lineScanner = Scanner(line).useDelimiter(",")
+                val lineIterator = line.split(",").iterator()
                 var x = 0
                 while (x < width) {
-                    val value = parse(lineScanner.next().trim())
+                    val value = parse(lineIterator.next().trim())
                     if (value != null) {
                         data.add(value)
                     }
@@ -110,7 +89,7 @@ class SudokuTextFormatParser {
         private fun readType(): SudokuType {
             var type = ""
             while (type.isEmpty()) {
-                type = scanner.nextLine()
+                type = lines.next()
             }
 
             return TypesRegistry.getTypeByName(type)
@@ -126,11 +105,11 @@ class SudokuTextFormatParser {
         }
 
         private fun isEmptyValue(input: String): Boolean {
-            return EMPTY_VALUE.matcher(input).matches()
+            return EMPTY_VALUE.matches(input)
         }
 
         private fun isNoCell(input: String): Boolean {
-            return NO_CELL.matcher(input).matches()
+            return NO_CELL.matches(input)
         }
 
         private fun asNumber(input: String): Int {
@@ -139,7 +118,7 @@ class SudokuTextFormatParser {
     }
 
     companion object {
-        private val EMPTY_VALUE = Pattern.compile("^_*$")
-        private val NO_CELL = Pattern.compile("^#*$")
+        private val EMPTY_VALUE = "^_*$".toRegex()
+        private val NO_CELL = "^#*$".toRegex()
     }
 }
--- core/src/test/kotlin/io/github/ilikeyourhat/kudoku/parsing/SudokuTextFormatParserTest.kt
+++ core/src/test/kotlin/io/github/ilikeyourhat/kudoku/parsing/SudokuTextFormatParserTest.kt
@@ -7,13 +7,8 @@ import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.matchers.throwable.shouldHaveMessage
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.io.TempDir
-import java.io.File
 
 class SudokuTextFormatParserTest {
-
-    @field:TempDir
-    lateinit var tempDir: File
 
     @Test
     fun `parse one sudoku from text`() {
@@ -33,47 +28,6 @@ class SudokuTextFormatParserTest {
         """.trimIndent()
 
         val sudoku = SudokuTextFormatParser().parseOne(text)
-
-        assertEquals(
-            Sudoku(
-                Classic9x9,
-                listOf(
-                    0, 0, 3, 0, 2, 0, 6, 0, 0,
-                    9, 0, 0, 3, 0, 5, 0, 0, 1,
-                    0, 0, 1, 8, 0, 6, 4, 0, 0,
-                    0, 0, 8, 1, 0, 2, 9, 0, 0,
-                    7, 0, 0, 0, 0, 0, 0, 0, 8,
-                    0, 0, 6, 7, 0, 8, 2, 0, 0,
-                    0, 0, 2, 6, 0, 9, 5, 0, 0,
-                    8, 0, 0, 2, 0, 3, 0, 0, 9,
-                    0, 0, 5, 0, 1, 0, 3, 0, 0
-                )
-            ),
-            sudoku
-        )
-    }
-
-    @Test
-    fun `parse one sudoku from file`() {
-        val file = File(tempDir, "test.txt")
-        file.writeText(
-            """
-            classic_9x9
-            _,_,3 ,_,2,_, 6,_,_
-            9,_,_ ,3,_,5, _,_,1
-            _,_,1 ,8,_,6, 4,_,_
-
-            _,_,8, 1,_,2, 9,_,_
-            7,_,_, _,_,_, _,_,8
-            _,_,6, 7,_,8, 2,_,_
-
-            _,_,2, 6,_,9, 5,_,_
-            8,_,_, 2,_,3, _,_,9
-            _,_,5, _,1,_, 3,_,_
-            """.trimIndent()
-        )
-
-        val sudoku = SudokuTextFormatParser().parseOne(file)
 
         assertEquals(
             Sudoku(
@@ -125,78 +79,6 @@ class SudokuTextFormatParserTest {
         """.trimIndent()
 
         val sudoku = SudokuTextFormatParser().parseMany(text)
-
-        assertEquals(
-            listOf(
-                Sudoku(
-                    Classic9x9,
-                    listOf(
-                        0, 0, 3, 0, 2, 0, 6, 0, 0,
-                        9, 0, 0, 3, 0, 5, 0, 0, 1,
-                        0, 0, 1, 8, 0, 6, 4, 0, 0,
-                        0, 0, 8, 1, 0, 2, 9, 0, 0,
-                        7, 0, 0, 0, 0, 0, 0, 0, 8,
-                        0, 0, 6, 7, 0, 8, 2, 0, 0,
-                        0, 0, 2, 6, 0, 9, 5, 0, 0,
-                        8, 0, 0, 2, 0, 3, 0, 0, 9,
-                        0, 0, 5, 0, 1, 0, 3, 0, 0
-                    )
-                ),
-                Sudoku(
-                    Classic9x9,
-                    listOf(
-                        2, 0, 0, 0, 8, 0, 3, 0, 0,
-                        0, 6, 0, 0, 7, 0, 0, 8, 4,
-                        0, 3, 0, 5, 0, 0, 2, 0, 9,
-
-                        0, 0, 0, 1, 0, 5, 4, 0, 8,
-                        0, 0, 0, 0, 0, 0, 0, 0, 0,
-                        4, 0, 2, 7, 0, 6, 0, 0, 0,
-
-                        3, 0, 1, 0, 0, 7, 0, 4, 0,
-                        7, 2, 0, 0, 4, 0, 0, 6, 0,
-                        0, 0, 4, 0, 1, 0, 0, 0, 3
-                    ),
-                )
-            ),
-            sudoku
-        )
-    }
-
-    @Test
-    fun `parse many sudoku from file`() {
-        val file = File(tempDir, "test.txt")
-        file.writeText(
-            """
-            classic_9x9
-            _,_,3 ,_,2,_, 6,_,_
-            9,_,_ ,3,_,5, _,_,1
-            _,_,1 ,8,_,6, 4,_,_
-
-            _,_,8, 1,_,2, 9,_,_
-            7,_,_, _,_,_, _,_,8
-            _,_,6, 7,_,8, 2,_,_
-
-            _,_,2, 6,_,9, 5,_,_
-            8,_,_, 2,_,3, _,_,9
-            _,_,5, _,1,_, 3,_,_
-            
-            classic_9x9
-            2,_,_, _,8,_, 3,_,_
-            _,6,_, _,7,_, _,8,4
-            _,3,_, 5,_,_, 2,_,9
-
-            _,_,_, 1,_,5, 4,_,8
-            _,_,_, _,_,_, _,_,_
-            4,_,2, 7,_,6, _,_,_
-            
-            3,_,1, _,_,7, _,4,_
-            7,2,_, _,4,_, _,6,_
-            _,_,4, _,1,_, _,_,3
-            """.trimIndent()
-        )
-
-        val sudoku = SudokuTextFormatParser().parseMany(file)
 
         assertEquals(
             listOf(
